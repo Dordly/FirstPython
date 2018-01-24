85605b72fcf116a9bfaf81eb701a7cc6994e70fa
Dordly <15730128494@163.com>
2018-01-24 15:37:35 +0800

    Python函数

---
+++ NewPython-One/List.py
@@ -0,0 +1,49 @@
+
+# classmates = ['Dordly','Bod','Tracy']
+# print(classmates)
+# print(classmates[0])
+# classmates.insert(1,'Jack')
+# print(classmates.pop())
+#
+# classmates[1] = 'Sara'
+#
+# print(classmates)
+
+# tuple
+# classmates = ('Dordly','Bod','Tracy')
+# print(classmates)
+
+# _*_ coding: utf-8 _*_
+
+# L = [
+#     ['Apple', 'Google', 'Microsoft'],
+#     ['Java', 'Python', 'Ruby', 'PHP'],
+#     ['Adam', 'Bart', 'Lisa']
+# ]
+# print(L)
+
+
+# age = 20
+#
+# if age >= 18:
+#     print('your age is', age)
+#     print('adult')
+
+
+# print(abs(100))
+
+import math
+
+def move(x,y,step, angle=0):
+    nx = x + step * math.cos(angle)
+    ny = y - step * math.sin(angle)
+    return nx,ny
+
+r = move(100,100,60, math.pi / 6)
+print(r)
+print(math.sqrt(2))
+
+def power(x):
+    return x * x
+
+print(power(20))
