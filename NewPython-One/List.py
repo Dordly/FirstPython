5e904d710a42f345de521f201b349f0164c2989f
Dordly <15730128494@163.com>
2018-02-28 10:23:47 +0800

    拉取项目测试

--- NewPython-One/List.py
+++ NewPython-One/List.py
@@ -30,21 +30,21 @@
 #     print('adult')
 
 
-# print(abs(100))
+ print(abs(100))
 
-# import math
-#
-# def move(x,y,step, angle=0):
-#     nx = x + step * math.cos(angle)
-#     ny = y - step * math.sin(angle)
-#     return nx,ny
-#
-# r = move(100,100,60, math.pi / 6)
-# print(r)
-# print(math.sqrt(2))
-#
-# def power(x):
-#     return x * x
-#
-# print(power(20))
+ import math
+
+ def move(x,y,step, angle=0):
+     nx = x + step * math.cos(angle)
+     ny = y - step * math.sin(angle)
+     return nx,ny
+
+ r = move(100,100,60, math.pi / 6)
+ print(r)
+ print(math.sqrt(2))
+
+ def power(x):
+     return x * x
+
+ print(power(20))
 
