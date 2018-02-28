930f84230753c0ed0c108c11b4d760ec66faf3de
Dordly <15730128494@163.com>
2018-02-28 14:22:55 +0800

    新增Python练习-变量和简单数据类型

--- PracticeProject/hello_work.py
+++ PracticeProject/hello_work.py
@@ -1,1 +1,18 @@
-print("Hello Python world!")
+message = "Hello Eric, would you like to learn some Python today?"
+print(message)
+
+name = "ada loveLace"
+print(name.upper()) # upper()--大写   lower() --小写
+print(name.lower())
+print(name[0].upper(),name.lower())
+
+famous_person = 'Albert Einstein once said,"A person who never made a mistake never tried anything new."'
+messageTwo = famous_person
+print(messageTwo)
+
+nameNot = "\t Dordly With Dordly \n"
+
+print(nameNot)
+print(nameNot.lstrip())
+print(nameNot.rstrip())
+print(nameNot.strip())
