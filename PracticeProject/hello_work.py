d8cfe079db40d3bca85e5537dfbbb19f2bed900b
Dordly <15730128494@163.com>
2018-03-01 15:43:47 +0800

    新增

--- PracticeProject/hello_work.py
+++ PracticeProject/hello_work.py
@@ -120,11 +120,38 @@
 # 	print(squares)
 
 
+# projects = []
+# for value in range(1,1000001):
+# 	project = value
+# 	projects.append(project)
+# 	print(project)
 
 
+# cars = ['audi', 'bmw', 'subaru', 'toyota']
+
+# for car in cars:
+# 	if car == 'bmw':
+# 		print(car.upper())
+# 	else:
+# 		print(car.title())
 
 
+# # 检查特定值是否不包含在列表中
+# banned_users = ['andrew', 'carolina', 'david']
+# user = 'marie'
 
+# if user not in banned_users:
+# 	print(user.title() + ", you can post a response if you wish.")
+
+
+age = 12
+
+if age < 4:
+	print("Your admission cost is $0.")
+elif age< 18:
+	print("Your admission cost is $5")
+else:
+	print("Your admission cost is $10")
 
 
 
