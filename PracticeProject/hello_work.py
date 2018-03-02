9b103babe8b82b5724336846fb04252e2a02d075
Dordly <15730128494@163.com>
2018-03-02 09:12:33 +0800

    拉取

--- PracticeProject/hello_work.py
+++ PracticeProject/hello_work.py
@@ -144,14 +144,17 @@
 # 	print(user.title() + ", you can post a response if you wish.")
 
 
-age = 12
+# age = 12
 
-if age < 4:
-	print("Your admission cost is $0.")
-elif age< 18:
-	print("Your admission cost is $5")
-else:
-	print("Your admission cost is $10")
+# if age < 4:
+# 	print("Your admission cost is $0.")
+# elif age< 18:
+# 	print("Your admission cost is $5")
+# else:
+# 	print("Your admission cost is $10")
+
+
+
 
 
 
