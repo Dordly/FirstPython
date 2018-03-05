7118f9249fdb659d87264137dfd842e7db1269ba
Dordly <15730128494@163.com>
2018-03-05 15:11:01 +0800

    使用Pygal模拟掷筛子并生成数据

---
+++ python-throw(掷骰子)/die_visual.py
@@ -0,0 +1,56 @@
+
+# # 投掷骰子
+# from die import Die 
+
+# # 创建一个D6
+# die = Die()
+
+# # 投掷几次骰子，并将结果存储在一个列表中
+# results = []
+
+# for roll_num in range(1000):
+# 	result = die.roll()
+# 	results.append(result)
+
+# # 分析结果
+# frequencies = []
+
+# for value in range(1, die.num_sides+1):
+# 	frequency = results.count(value)
+# 	frequencies.append(frequency)
+
+# print(frequencies)
+
+
+# 绘制直方图
+
+import pygal
+from die import Die 
+
+# 创建一个D6
+die = Die()
+
+# 投掷几次骰子，并将结果存储在一个列表中
+results = []
+
+for roll_num in range(1000):
+	result = die.roll()
+	results.append(result)
+# 分析结果
+frequencies = []
+
+for value in range(1, die.num_sides+1):
+	frequency = results.count(value)
+	frequencies.append(frequency)
+
+# 对结果可视化
+hist = pygal.Bar()
+
+hist.title = "Results of rolling one D6 1000 times."
+hist.x_labels = ['1', '2', '3', '4', '5', '6']
+hist.x_title = "Result"
+hist.y_title = "Frequency of Result"
+
+hist.add('D6', frequencies)
+hist.render_to_file('die_visual.svg')
+
