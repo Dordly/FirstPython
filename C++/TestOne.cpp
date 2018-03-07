f01a4792121d68989960c914b1349eaa412ca5aa
Dordly <15730128494@163.com>
2018-03-07 17:55:10 +0800

    新增C++练习以及基础知识学习

---
+++ C++/TestOne.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	short s; 
+	int i;
+	long l;
+	float f;
+	double d;
+
+	s = 10;
+	i = 10000;
+	l = 100200;
+	f = 203.39;
+	d = 23242.1234;
+
+	cout << "short s :" << s << endl;
+	cout << "int i :" << i << endl;
+	cout << "long l :" << l << endl;
+	cout << "float f :" << f << endl;
+	cout << "double d :" << d << endl;
+}
