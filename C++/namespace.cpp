b319056ed68d88f014a78b43a2fe00e0b931d416
Dordly <15730128494@163.com>
2018-03-09 18:06:54 +0800

    C++

---
+++ C++/namespace.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+ 
+class printData 
+{
+   public:
+      void print(int i) {
+        cout << "整数为: " << i << endl;
+      }
+ 
+      void print(double  f) {
+        cout << "浮点数为: " << f << endl;
+      }
+ 
+      void print(string c) {
+        cout << "字符串为: " << c << endl;
+      }
+};
+ 
+int main(void)
+{
+   printData pd;
+ 
+   // 输出整数
+   pd.print(5);
+   // 输出浮点数
+   pd.print(500.263);
+   // 输出字符串
+   pd.print("Hello C++");
+ 
+   return 0;
+}
